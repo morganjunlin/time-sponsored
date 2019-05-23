c6e84452be87ea05abb75e51bfefc641295eecc2
Jongho Kim <april9288@gmail.com>
2019-05-22 21:38:53 -0700

    aded order comp test suites

---
+++ client/components/Order/Order.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Order from './Order';
+
+describe('testing Order component', ()=> {
+
+	it('should return a single-node wrapper.', ()=> {
+		expect(shallow(<Order />).length).toEqual(1)
+	})
+
+	it('should grab a snapshot of the component', ()=> {
+		const component = renderer.create(<Order />)
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	})
+
+})
