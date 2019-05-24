28ac02134482c346b9008951bf05b5a1df0729d9
Jongho Kim <april9288@gmail.com>
2019-05-23 21:37:41 -0700

    almost done with the sponsor component

--- client/components/Sponsor/Sponsor.js
+++ client/components/Sponsor/Sponsor.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import './Sponsor.css';
-import {fake_data} from './fake_data';
 import CardContainer from './CardContainer/CardContainer';
 
 class Sponsor extends React.Component{
-	constructor(props){
-		super(props)
-		this.state={
-			data: null
-		}
-	}
-
-	componentDidMount(){
-		this.setState({data:fake_data})
-	}
 
 	render(){
 		return(
 			<section className="sponsor-container">	
 				<p id="sponsor-container">Sponsored restaurants in your area</p>
-				<CardContainer data={this.state.data}/>
+				<CardContainer/>
 			</section>
 			)
 	}
