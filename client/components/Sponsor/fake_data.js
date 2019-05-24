d5fa4e656528aef381451b41b3deeb2b7e7c748b
Jongho Kim <april9288@gmail.com>
2019-05-23 17:36:40 -0700

    edited

---
+++ client/components/Sponsor/fake_data.js
@@ -0,0 +1,111 @@
+export const fake_data = [
+	{
+		image:"https://source.unsplash.com/280x140/?food",
+		name:"Waba Grill",
+		category: "Asian, Bowls",
+		waiting: "25-35 mins",
+		extra: "$0 min",
+		rate: 4,
+		count: 38
+	},
+	{
+		image:"https://source.unsplash.com/280x140/?food",
+		name:"Waba Grill",
+		category: "Asian, Bowls",
+		waiting: "25-35 mins",
+		extra: "$0 min",
+		rate: 4,
+		count: 38
+	},
+	{
+		image:"https://source.unsplash.com/280x140/?food",
+		name:"Waba Grill",
+		category: "Asian, Bowls",
+		waiting: "25-35 mins",
+		extra: "$0 min",
+		rate: 4,
+		count: 38
+	},
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+	// {
+	// 	image:"https://source.unsplash.com/280x140/?food",
+	// 	name:"Waba Grill",
+	// 	category: "Asian, Bowls",
+	// 	waiting: "25-35 mins",
+	// 	extra: "$0 min",
+	// 	rate: 4,
+	// 	count: 38
+	// },
+
+]
