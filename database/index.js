42b3d778f6aaddd506c3c8bd9c279f8676f3e398
Jongho Kim <april9288@gmail.com>
2019-05-29 15:45:34 -0700

    ready to deploy

--- database/index.js
+++ database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/grubhub', {
+let DB_URL = (process.env.NODE_ENV === 'development') ? "mongodb://127.0.0.1:27017/grubhub" : "mongodb+srv://hackreactortest:hackreactortest@cluster0-7iwj5.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(DB_URL, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
     useFindAndModify: false
